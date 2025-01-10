99d09ea4e40d94830b4032c0ae284acb5a005bdf
Carolina <carolina.kaji@gmail.com>
2025-01-10 12:13:33 -0300

    Adicionado Scanner para inserir a idade e logica para definir se maior de idade

--- semana_01/src/maioridade/Main.java
+++ semana_01/src/maioridade/Main.java
@@ -1,9 +1,22 @@
 package maioridade;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// Verifique se uma pessoa é maior de idade com base na idade fornecida.
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite uma idade:");
+		int idade = scan.nextInt();
+		scan.close();
+		
+		if(idade >= 18) {
+			System.out.println("Sim, você é maior de idade");
+		} else {
+			System.out.println("Não, você não é maior de idade");
+		}
 
 	}
 
