656c2d77e339d1f50fc4630e90c2fec79ac0f187
Carolina <carolina.kaji@gmail.com>
2025-01-13 16:13:48 -0300

    Verificacao de entrada de valores para cor no sistema RGB

---
+++ semana_01/src/validacao_corRGB/Main.java
@@ -0,0 +1,31 @@
+package validacao_corRGB;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Faça um programa que receba três valores inteiros representando os valores de **R**, **G** e **B**. O programa deve verificar se esses valores formam uma cor válida no sistema RGB. As regras são:
+		 * - Cada valor deve estar entre 0 e 255.
+		 * Se válido, exiba "Cor válida". Caso contrário, exiba "Cor inválida".
+		 */
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite o valor entre 0 - 255 para R (red)");
+		int red = scan.nextInt();
+		System.out.println("Digite o valor entre 0 - 255 para G (green)");
+		int green = scan.nextInt();
+		System.out.println("Digite o valor entre 0 - 255 para B (blue)");
+		int blue = scan.nextInt();
+		scan.close();
+		
+
+		if (red<0 || red>255 || green<0 || green>255 || blue<0 || blue>255) {
+			System.out.println("Cor invalida");
+		} else {
+			System.out.println("Cor valida");
+		}
+	}
+
+}
