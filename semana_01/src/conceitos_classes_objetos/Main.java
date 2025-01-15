de42bfe571751ebb630c948c8ddf725a8a58b912
Carolina <carolina.kaji@gmail.com>
2025-01-15 20:32:02 -0300

    Criando classe Pessoa e instanciando o objeto

---
+++ semana_01/src/conceitos_classes_objetos/Main.java
@@ -0,0 +1,14 @@
+package conceitos_classes_objetos;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Pessoa pessoa1 = new Pessoa("John", 30);
+		pessoa1.detalhePessoa();
+		
+		Pessoa pessoa2 = new Pessoa("Mary", 33);
+		pessoa2.detalhePessoa();
+	}
+
+}
